b06368dba035f57bf1677cd1eb2e7d2469befc59
Stéphane RAULT <stephane.rault@radicalspam.org>
2020-04-17 17:37:58 +0200

    fixe asgi import

--- mce_django_server/asgi.py
+++ mce_django_server/asgi.py
@@ -1,8 +1,8 @@
 import os
 
-from django.core.asgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
 
 if not os.environ.get('DJANGO_SETTINGS_MODULE'):
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', ' mce_django_server.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mce_django_server.settings.dev')
 
 application = get_asgi_application()
